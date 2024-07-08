8c2ca69bdbb55481fb4d55dbc1ca07445db43e34
Konrad Hylton <konradhylton@gmail.com>
2024-07-07 23:08:53 -0500

    Update BACKEND_HOST to use environment variable or default to localhost:3001

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,2 @@
-export const BACKEND_HOST = "http://127.0.0.1:3000";
-
-//export const BACKEND_HOST = process.env.NEXT_PUBLIC_BACKEND_HOST || "http://127.0.0.1:3001";
+export const BACKEND_HOST =
+  process.env.NEXT_PUBLIC_BACKEND_HOST || "http://127.0.0.1:3001";
